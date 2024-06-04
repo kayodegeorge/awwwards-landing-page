3565b875c576b150f260cf48c4cac89db566d8ee
kayodegeorge <gkayode75@gmail.com>
2024-06-04 20:54:35 +0100

    first?

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -1,30 +1,42 @@
-import styles from './style.module.scss';
-import { useInView, motion } from 'framer-motion';
-import { useRef } from 'react';
-import { slideUp, opacity } from './animation';
-import Rounded from '../../common/RoundedButton';
+import styles from './style.module.scss'
+import { useInView, motion } from 'framer-motion'
+import { useRef } from 'react'
+import { slideUp, opacity } from './animation'
+import Rounded from '../../common/RoundedButton'
 export default function index() {
-
-    const phrase = "Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.";
-    const description = useRef(null);
-    const isInView = useInView(description)
-    return (
-        <div ref={description} className={styles.description}>
-            <div className={styles.body}>
-                <p>
-                {
-                    phrase.split(" ").map( (word, index) => {
-                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
-                    })
-                }
-                </p>
-                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>The combination of my passion for design, code & interaction positions me in a unique place in the web design world.</motion.p>
-                <div data-scroll data-scroll-speed={0.1}>
-                    <Rounded className={styles.button}>
-                        <p>About me</p>
-                    </Rounded>
-                </div>
-            </div>
+  const phrase =
+    'Focused on building software products with great user experience; building interfaces that are thoughful, intuitive and generally accessible. I have with experience in working with both front-end and back-end JavaScript frameworks and libraries for personal projects, client-facing products, and teams with fast development cycles. '
+  const description = useRef(null)
+  const isInView = useInView(description)
+  return (
+    <div ref={description} className={styles.description}>
+      <div className={styles.body}>
+        <p>
+          {phrase.split(' ').map((word, index) => {
+            return (
+              <span key={index} className={styles.mask}>
+                <motion.span
+                  variants={slideUp}
+                  custom={index}
+                  animate={isInView ? 'open' : 'closed'}
+                  key={index}
+                >
+                  {word}
+                </motion.span>
+              </span>
+            )
+          })}
+        </p>
+        <motion.p variants={opacity} animate={isInView ? 'open' : 'closed'}>
+          The combination of my passion for design, code & interaction positions
+          me in a unique place in the web design world.
+        </motion.p>
+        <div data-scroll data-scroll-speed={0.1}>
+          <Rounded className={styles.button}>
+            <p>About me</p>
+          </Rounded>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
